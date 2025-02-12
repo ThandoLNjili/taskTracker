887115283d4fd2c9155f2c78bf14ad5e0ba25f88
Thando <thandolnjili@gmail.com>
2025-02-12 17:06:06 +0200

    added updateTask, filechecker, reworked addTask

--- index.js
+++ index.js
@@ -1,24 +1,61 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = 'tasks.json';
 const args = process.argv.slice(2);
 
-let data = [];
-
-if (fs.existsSync(path)) {
-    data = JSON.parse(fs.readFileSync(path, "utf8"));
+async function ensureFileExists(path) {
+    try {
+        await fs.access(path);
+    } catch {
+        await fs.writeFile(path, JSON.stringify([], null, 2));
+        console.log(`Created new file: ${filePath}`);
+    }
 }
 
-const newId = data.length > 0 ? Math.max(...data.map(item => item.id)) + 1 : 1;
-
-function addTask(newId, newTask){
-    let task = {id: newId, task: newTask}
-    data.push(task);
-    fs.writeFileSync(path, JSON.stringify(data, null, 2));
-    console.log(`Task added successfully (ID:${newId})`);
+async function addTask(newTask, tasks) {
+    try {
+        const newId = tasks.length > 0 ? Math.max(...tasks.map(item => item.id)) + 1 : 1;
+        newTask = {id: newId, task: newTask}
+        tasks.push(newTask);
+        await fs.writeFile(path, JSON.stringify(tasks, null, 2));
+        console.log(`Task added successfully (ID:${newId})`);
+    } catch (err) {
+        console.log("Error adding task:", err);
+    }
 }
 
-if (args[0].toLowerCase() == 'add') {
-    addTask(newId, args[1])
+async function updateTask(id, updatedTask, tasks) {
+    console.log(updatedTask)
+    try {
+        const taskIndex = tasks.findIndex(task => task.id == id);
+        
+        if (taskIndex === -1) {
+            console.log(`Task with ID ${id} not found.`);
+            return;
+        }
+
+        tasks[taskIndex].task = updatedTask;
+        await fs.writeFile(path, JSON.stringify(tasks, null, 2));
+        console.log("Task updated successfully!");    
+    } catch (err) {
+        console.log("Error updating task:", err);
+    }
 }
+
+(async () => {
+    try {
+
+        await ensureFileExists(path);
+        const fileData = await fs.readFile(path, 'utf8');
+        const tasks = JSON.parse(fileData);
+
+        if (args[0].toLowerCase() == 'add') {
+            await addTask(args[1], tasks);
+        } else if (args[0].toLowerCase() == 'update') {
+            await updateTask(args[1], args[2], tasks);
+        }
+    } catch (err) {
+        console.error("Error reading file:", err);
+    }
+})();
